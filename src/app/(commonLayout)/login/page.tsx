9a8ac8fd3ba4e3e316e081b40b5a0b239af574ae
faishal-developer <foysal.professional@gmail.com>
2023-10-20 07:13:32 +0600

    fine

--- src/app/(commonLayout)/login/page.tsx
+++ src/app/(commonLayout)/login/page.tsx
@@ -27,7 +27,8 @@ type FormValues = {
 const LoginPage = () => {
   const [userLogin,result] = useLoginMutation();
   const router:any = useRouter();
-  const targetRoute = '/home'; 
+  const searchParams = new URLSearchParams(window.location.search);
+  const targetRoute = searchParams.get('targetRoute') || '/home'; 
   const dispatch = useAppDispatch();
   
   const onSubmit: SubmitHandler<FormValues> = async (data: any) => {
